d4deba0286e2f547fc28c13a02dd38bb8d995c30
boringtutor <boringtutor@gmail.com>
2024-08-29 20:59:59 -0700

    fixed the theme issue on every page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -19,25 +19,28 @@ export default function Error({
   }, [error]);
 
   const handleReset = () => {
-    console.log("Resetting...");
     reset();
   };
-
   return (
-    <div className=" flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-gray-900 to-gray-800 text-white relative overflow-hidden">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-gray-900 to-gray-800 text-white relative overflow-hidden">
       <Background />
-      <h1 className="text-4xl sm:text-5xl font-bold mb-6 bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-600">
-        Something went wrong!
-      </h1>
-      <p className="text-lg mb-8">An unexpected error has occurred.</p>
-
-      <Button
-        onClick={handleReset}
-        size="lg"
-        className="bg-gradient-to-r from-purple-500 to-pink-500 hover:from-purple-600 hover:to-pink-600 transition-all duration-200"
-      >
-        Try Again <ArrowRight className="ml-2" />
-      </Button>
+      <div className="z-10 relative">
+        <h1 className="flex justify-center w-full text-4xl sm:text-5xl font-bold mb-8 bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-600">
+          Something went wrong!
+        </h1>
+        <p className="flex justify-center text-xl mb-8">
+          An unexpected error has occurred.
+        </p>
+        <div className="space-y-4 flex justify-center">
+          <Button
+            onClick={handleReset}
+            size="lg"
+            className="bg-gradient-to-r from-purple-500 to-pink-500 hover:from-purple-600 hover:to-pink-600 transition-all duration-200"
+          >
+            Try Again <ArrowRight className="ml-2" />
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,38 +4,37 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import React from "react";
 import { useRouter } from "next/navigation";
+import { ArrowRight } from "lucide-react";
 
 export default function NotFound() {
   const router = useRouter();
 
+  const handleClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    console.log("Link clicked");
+    router.push("/");
+  };
+
   return (
-    <div className=" flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-gray-900 to-gray-800 text-white relative overflow-hidden">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-gray-900 to-gray-800 text-white relative overflow-hidden">
       <Background />
-      <h1 className="text-4xl sm:text-5xl font-bold mb-6 bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-600">
-        404 - Page Not Found
-      </h1>
-      <p className="text-xl mb-8">
-        Oops! The page you are looking for does not exist.
-      </p>
-      <div className="pointer-events-auto">
-        <Link
-          href="/"
-          className="text-lg text-blue-500 hover:text-white border border-blue-500 hover:bg-blue-500 transition-colors duration-300 py-2 px-4 rounded"
-        >
-          Return Home
-        </Link>
+      <div className="z-10 relative">
+        <h1 className="text-4xl sm:text-5xl font-bold mb-6 bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-600">
+          404 - Page Not Found
+        </h1>
+        <p className="text-xl mb-8">
+          Oops! The page you are looking for does not exist.
+        </p>
+        <div className="space-y-4 flex justify-center">
+          <Button
+            onClick={handleClick}
+            size="lg"
+            className="bg-gradient-to-r from-purple-500 to-pink-500 hover:from-purple-600 hover:to-pink-600 transition-all duration-200"
+          >
+            Return Home <ArrowRight className="ml-2" />
+          </Button>
+        </div>
       </div>
-      <Link
-        onClick={(e) => {
-          e.preventDefault();
-          console.log("Link clicked");
-          // You can add router.push('/') here if needed
-        }}
-        href="/"
-        className="text-lg text-blue-500 hover:text-white border border-blue-500 hover:bg-blue-500 transition-colors duration-300 py-2 px-4 rounded"
-      >
-        Return Home
-      </Link>
     </div>
   );
 }
